7384d2d740057684b820950d36af38b6e4516773
Franklyn-N <66632010+Franklyn-N@users.noreply.github.com>
2022-03-25 15:15:39 +0100

    added config file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const express = require('express');
 const userRouter = require("./routes/index.js");
+const { config } = require('./config/config.js');
+
 
 const app = express();
 app.use(express.json())
@@ -8,13 +10,13 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/', userRouter);
 
-let port = 8080;
+
 
 app.get("/", (req, res) => {
     res.status(200).json({message: "Wow everything is working fine!"})
   })
 
 const httpServer = http.createServer(app);
-httpServer.listen(port, () => {
-    console.log(`Server running on ${port}`)
+httpServer.listen(config.port, () => {
+    console.log(`Server running on ${config.port}`);
 });
--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,12 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const PORT = process.env.PORT;
 const SUBCRIPTION_KEY = process.env.SUBCRIPTION_KEY;
 
 const config = {
-    sub_key: SUBCRIPTION_KEY
+    sub_key: SUBCRIPTION_KEY,
+    port: PORT
 };
 
 module.exports = {config};
